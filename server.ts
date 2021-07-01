9f5a44ca5a996dc91487fb02d7abef4f6838ac8e
VNWK <vcharrington3@gmail.com>
2021-07-01 17:27:59 -0500

    Update: implemented mongoose

--- server.ts
+++ server.ts
@@ -1,8 +1,23 @@
-import express from 'express';
-// rest of the code remains same
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
 const app = express();
 const PORT = 8000;
-app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+
+app.use(cors());
+
+mongoose
+  .connect("mongodb://localhost:27017/washly", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB connection successful"))
+  .catch((err: any) => console.error("DB connection failed" + err));
+
+// rest of the code remains same
+
+app.get("/", (req, res) => res.send("Express + TypeScript Server"));
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });
