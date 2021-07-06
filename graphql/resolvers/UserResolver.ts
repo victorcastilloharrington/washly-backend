521c6ba8438fd19d522b1b391865607326f0fb75
VNWK <vcharrington3@gmail.com>
2021-07-06 15:04:00 -0500

    C: Simplified user resolver

--- graphql/resolvers/UserResolver.ts
+++ graphql/resolvers/UserResolver.ts
@@ -10,26 +10,12 @@ import { User, UserInput } from "../schemas/User";
 import UserModel from "../../models/user";
 import { Location } from "../schemas/Location";
 import LocationModel from "../../models/location";
+
+import createBaseResolver from "./BaseResolver";
+
+const UserBaseResolver = createBaseResolver("User", User, UserModel, UserInput);
 @Resolver((of) => User)
-export class UserResolver {
-  @Query((returns) => [User], { nullable: true })
-  async users(): Promise<User[]> {
-    return await UserModel.find({}).lean();
-  }
-
-  @Query((returns) => User)
-  async user(@Arg("id") id: string): Promise<User> {
-    return await UserModel.findById(id).lean();
-  }
-
-  @Mutation((returns) => User)
-  async addUser(
-    @Arg("userInput") { name, email, password }: UserInput
-  ): Promise<User> {
-    const user = await UserModel.create({ name, email, password });
-    return user;
-  }
-
+export class UserResolver extends UserBaseResolver {
   @FieldResolver((returns) => [Location])
   async locations(@Root() user: User): Promise<Location[]> {
     const location = await LocationModel.find({ userId: user._id });
